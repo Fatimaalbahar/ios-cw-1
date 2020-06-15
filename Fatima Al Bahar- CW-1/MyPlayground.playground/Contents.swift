637521580b8f8e42cef76aadf8a4010e7dc78d38
Fatimaalbahar <albaharfatema18@gmail.com>
2020-06-15 23:02:52 +0300

    classwork

---
+++ Fatima Al Bahar- CW-1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+let name = "Fatima AlBahar"
+var hobby1 = "horse back riding"
+var hobby2 = "shopping"
+var age = 18
+var height = 1.64
+print("My name is \(name), my hobbies include \(hobby1) and \(hobby2), I am \(age) years old, and I am \(height) meters tall. ")
+let friendnname = "Fatma AlHamad"
+var friendhobby1 = "cooking"
+var friendhobby2 = "swimming"
+var friendage = 17
+var friendheight = 1.66
+var age_difference = friendage - age
+age == friendage
+age > friendage
+age < friendage
+age <= friendage
+age >= friendage
+age != friendage
+
+hobby1 == friendhobby1
+hobby1 != friendhobby1
+hobby2 == friendhobby2
+hobby2 != friendhobby2
+hobby1 != friendhobby2
+hobby2 == friendhobby1
+hobby2 != friendhobby1
+
+height == friendheight
+height < friendheight
+height > friendheight
+height >= friendheight
+height <= friendheight
+height != friendheight
+
+
+
+
